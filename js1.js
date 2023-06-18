c75907264975d26ab159acceb6b88ece2718de97
Razeen Shaheed <78610722+Razepriv@users.noreply.github.com>
2023-06-18 15:11:49 +0530

    initial commit

---
+++ js1.js
@@ -0,0 +1,27 @@
+function add(){
+    var x = Number(document.getElementById("fnum").value);
+    var y = Number(document.getElementById("snum").value);
+    sum = x + y ;
+    alert(sum);
+}
+
+function sub(){
+    var x = Number(document.getElementById("fnum").value);
+    var y = Number(document.getElementById("snum").value);
+    sum = x - y ;
+    alert(sum);
+}
+
+function multipy(){
+    var x = Number(document.getElementById("fnum").value);
+    var y = Number(document.getElementById("snum").value);
+    sum = x * y ;
+    alert(sum);
+}
+
+function divide(){
+    var x = Number(document.getElementById("fnum").value);
+    var y = Number(document.getElementById("snum").value);
+    sum = x / y ;
+    alert(sum);
+}
